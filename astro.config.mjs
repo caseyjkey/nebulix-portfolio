889cbbab2b6ce4689614458decacd22f693034a9
Casey JG Key <casey.key@pm.me>
2023-10-12 20:54:30 -0600

    setting up nebulix

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://main--transcendent-tulumba-e9b0c7.netlify.app",
   integrations: [
     mdx({}),
     sitemap(),
